071a6901594657aa7e7c9f7479fac29a2e01544b
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2022-11-14 14:42:12 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,28 +7,42 @@ import VatRateField from './VatRateField';
 function App() {
   const [netPrice, setNetPrice] = useState(0.0);
   const [grossPrice, setGrossPrice] = useState(0.0);
+  const [vatToPay, setVatToPay] = useState(0.0);
+  const [vatRate, setVatRate] = useState(20.0);
 
   const handleNetPriceChange = (price) => {
-    const gross_price = price * ((20 / 100) + 1);
+    const gross_price = price * ((vatRate / 100) + 1);
     setNetPrice(price);
     setGrossPrice(gross_price);
-    // TODO: calc vat to pay and set state
+    // calc vat to pay and set state
+    setVatToPay(gross_price - price);
   }
   
   const handleGrossPriceChange = (price) => {
-    const net_price = price / ((20 / 100) + 1);
+    const net_price = price / ((vatRate / 100) + 1);
     setNetPrice(net_price);
     setGrossPrice(price);
-    // TODO: calc vat to pay and set state
+    // calc vat to pay and set state
+    setVatToPay(price - net_price);
+  }
+
+  const handleVatRateChanged = (rate) => {
+    setVatRate(rate);
+    updatePrices();
+  }
+
+  
+  const updatePrices = () => {
+    handleNetPriceChange(netPrice);
   }
 
   return (
     <div className="header field" >
         VAT CALCULATOR
         <div className="pale-green-border" >
-          <VatRateField />
+          <VatRateField vatRateChanged={handleVatRateChanged} updatePrices={updatePrices} value={vatRate}  />
           <PriceEntryField label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0? "": netPrice} customstyle="field" />
-          <DisplayBlock label="VAT to pay: " value="4"  customstyle="field" />
+          <DisplayBlock label="VAT to pay: " value={vatToPay}  customstyle="field" />
           <PriceEntryField label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0? "": grossPrice} customstyle="field" />
         </div>
     </div>
--- src/VatRateField.jsx
+++ src/VatRateField.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useEffect } from "react";
 
 const VatRateField = (props) => {
+    const { updatePrices } = props;
+
+    useEffect(() => {
+        updatePrices();
+    }, [props.value, updatePrices ] );
+
     return (
         <div className={props.customstyle}>
             VAT Rate:
-            <select>
+            <select value={props.value} onChange={(event) => {props.vatRateChanged(event.target.value);}}>
                 <option value="20">20%</option>
                 <option value="15">15%</option>
                 <option value="12.5">12.5%</option>
